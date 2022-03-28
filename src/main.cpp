23f4b96a1119729df0b8c280bdaf299d831d4c8b
Mark van de Ruit <markvanderuit@gmail.com>
2022-03-28 22:47:32 +0200

    Set up initial required header files

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+#include <glad/glad.h>
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+
+int main(int argc, char** argv) {
+  std::cout << "Hello world!" << std::endl;
+  return 0;
+}
