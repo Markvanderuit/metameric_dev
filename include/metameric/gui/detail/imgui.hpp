34875672b2b09eda9862be2eb22fdc36bf0549e2
Mark van de Ruit <markvanderuit@gmail.com>
2022-05-21 01:06:17 +0200

    Added ImVec2/4 -> glm::vec2/4 conversion

--- include/metameric/gui/detail/imgui.hpp
+++ include/metameric/gui/detail/imgui.hpp
@@ -1,17 +1,7 @@
 #pragma once
 
 #include <metameric/core/utility.hpp>
-#include <metameric/core/detail/eigen.hpp>
 #include <small_gl/window.hpp>
-#include <glm/glm.hpp>
-
-// Interoperability with eigen's types
-// #define IM_VEC2_CLASS_EXTRA                                     \
-//   ImVec2(const met::Array2f &v) : x(v[0]), y(v[1]) { }          \
-//   ImVec2(const met::Array2i &v) : x(v[0]), y(v[1]) { }          \
-//   operator met::Array2f() const { return { x, y }; }            \
-//   operator met::Array2i() const { return {                      \
-//     static_cast<int>(x), static_cast<int>(y) }; }
 
 #define IM_VEC2_CLASS_EXTRA                                   \
   ImVec2(const glm::ivec2 &v) : x(v[0]), y(v[1]) { }          \
@@ -20,21 +10,19 @@
   operator glm::ivec2() const { return {                      \
     static_cast<int>(x), static_cast<int>(y) }; }
 
-// #define IM_VEC2_CLASS_EXTRA                                                     \
-//         constexpr ImVec2(const MyVec2& f) : x(f.x), y(f.y) {}                   \
-//         operator MyVec2() const { return MyVec2(x,y); }
+#define IM_VEC4_CLASS_EXTRA                                   \
+  ImVec4(const glm::ivec4 &v) :                               \
+  x(v[0]), y(v[1]), z(v[2]), w(v[3]) { }                      \
+  ImVec4(const glm::vec4 &v)                                  \
+  : x(v[0]), y(v[1]), z(v[2]), w(v[3]) { }                    \
+  operator glm::vec4() const { return { x, y, z, w }; }       \
+  operator glm::ivec4() const { return {                      \
+    static_cast<int>(x), static_cast<int>(y),                 \
+    static_cast<int>(z), static_cast<int>(w)}; }
 
 #include <imgui.h>
 
-// Simple guard statement syntactic sugar
-#define guard(expr,...) if (!(expr)) { return __VA_ARGS__ ; }
-
 namespace ImGui {
-  // template <typename T>
-  // constexpr met::Array2i to_array(T t) { 
-  //   return { static_cast<int>(t[0]), static_cast<int>(t[1]) };
-  // }
-  
   template <typename T>
   constexpr void * to_ptr(T t) { 
     return (void *) static_cast<size_t>(t);
@@ -53,8 +41,7 @@ namespace ImGui {
     }
 
     ScopedStyleVar(ImGuiStyleVar var, const glm::ivec2 &v) {
-      // PushStyleVar(var, to_imvec2(v));
-      PushStyleVar(var, ImVec2(v));
+      PushStyleVar(var, v);
     }
 
     ~ScopedStyleVar() {
