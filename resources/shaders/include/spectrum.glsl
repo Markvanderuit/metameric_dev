d3515391a8c9d86ac73d6a48212f65b16e9dd662
Mark van de Ruit <markvanderuit@gmail.com>
2022-06-27 16:05:56 +0200

    Work on shader backend

--- resources/shaders/include/spectrum.glsl
+++ resources/shaders/include/spectrum.glsl
@@ -15,39 +15,92 @@ const float wavelength_ssize = wavelength_range / float(wavelength_samples);
 const float wavelength_ssinv = float(wavelength_samples) / wavelength_range;
 
 /* Base Spectrum object */
-struct Spectrum {
-  float values[wavelength_samples];
-};
+#define Spectrum float[wavelength_samples]
+// struct Spectrum {
+//   float values[wavelength_samples];
+// };
 
-Spectrum add(in Spectrum a, in Spectrum b, in uint i) {
+Spectrum constr_spectrum(in float constant) {
   Spectrum s;
-  s.values[i] = a.values[i] + b.values[i];
-  return s;
-}
-
-Spectrum sub(in Spectrum a, in Spectrum b, in uint i) {
-  Spectrum s;
-  s.values[i] = a.values[i] - b.values[i];
-  return s;
-}
-
-Spectrum mul(in Spectrum a, in Spectrum b, in uint i) {
-  Spectrum s;
-  s.values[i] = a.values[i] * b.values[i];
-  return s;
-}
-
-Spectrum div(in Spectrum a, in Spectrum b, in uint i) {
-  Spectrum s;
-  s.values[i] = a.values[i] / b.values[i];
-  return s;
-}
-
-float mean(in Spectrum s) {
-  float f = 0.f;
   for (uint i = 0; i < wavelength_samples; ++i)
-    f += s.values[i];
-  return f;
+    s[i] = constant;
+  return s;
+}
+
+Spectrum add(in Spectrum a, in Spectrum b) {
+  Spectrum s;
+  for (uint i = 0; i < wavelength_samples; ++i)
+    s[i] = a[i] + b[i];
+  return s;
+}
+
+Spectrum div(in Spectrum a, in Spectrum b) {
+  Spectrum s;
+  for (uint i = 0; i < wavelength_samples; ++i)
+    s[i] = a[i] / b[i];
+  return s;
+}
+
+Spectrum maxv(in Spectrum a, in Spectrum b) {
+  Spectrum s;
+  for (uint i = 0; i < wavelength_samples; ++i)
+    s[i] = max(a[i], b[i]);
+  return s;
+}
+
+Spectrum maxv(in Spectrum a, in float b) {
+  Spectrum s;
+  for (uint i = 0; i < wavelength_samples; ++i)
+    s[i] = max(a[i], b);
+  return s;
+}
+
+Spectrum minv(in Spectrum a, in Spectrum b) {
+  Spectrum s;
+  for (uint i = 0; i < wavelength_samples; ++i)
+    s[i] = min(a[i], b[i]);
+  return s;
+}
+
+Spectrum minv(in Spectrum a, in float b) {
+  Spectrum s;
+  for (uint i = 0; i < wavelength_samples; ++i)
+    s[i] = min(a[i], b);
+  return s;
+}
+
+// Spectrum sub(in Spectrum a, in Spectrum b, in uint i) {
+//   Spectrum s;
+//   s.values[i] = a.values[i] - b.values[i];
+//   return s;
+// }
+
+// Spectrum mul(in Spectrum a, in Spectrum b, in uint i) {
+//   Spectrum s;
+//   s.values[i] = a.values[i] * b.values[i];
+//   return s;
+// }
+
+// Spectrum div(in Spectrum a, in Spectrum b, in uint i) {
+//   Spectrum s;
+//   s.values[i] = a.values[i] / b.values[i];
+//   return s;
+// }
+
+// float mean(in Spectrum s) {
+//   float f = 0.f;
+//   for (uint i = 0; i < wavelength_samples; ++i)
+//     f += s.values[i];
+//   return f;
+// }
+
+float wavelength_at_index(uint i) {
+  return wavelength_min + wavelength_ssize * (float(i) + .5f);
+}
+
+uint index_at_wavelength(float wvl) {
+  uint i = uint((wvl - wavelength_min) * wavelength_ssinv);
+  return min(i, wavelength_samples - 1);
 }
 
 vec3 test_fun(in vec3 v) {
