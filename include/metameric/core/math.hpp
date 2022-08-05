4cc591760045f2b1cbe30e5dbec70c0f0c501a95
Mark van de Ruit <markvanderuit@gmail.com>
2022-08-05 16:39:34 +0200

    Added perspective/lookat functions for eigen

--- include/metameric/core/math.hpp
+++ include/metameric/core/math.hpp
@@ -5,6 +5,18 @@
 
 namespace met {
   // Shorthand unsigned types
-  using uint = unsigned int;
+  using uint   = unsigned int;
   using ushort = unsigned short;
 } // namespace met
+
+namespace Eigen {
+  Affine3f lookat_rh(const Vector3f &eye,
+                     const Vector3f &cen,
+                     const Vector3f &up);
+
+  Affine3f lookat_lh(const Vector3f &eye,
+                     const Vector3f &cen,
+                     const Vector3f &up);
+
+  Projective3f perspective_rh_no(float fovy, float aspect, float near, float far);
+} // namespace Eigen
---
+++ src/core/math.cpp
@@ -0,0 +1,52 @@
+#include <metameric/core/math.hpp>
+
+namespace met {
+} // namespace met
+
+namespace Eigen {
+  Affine3f lookat_rh(const Vector3f &eye,
+                     const Vector3f &cen,
+                     const Vector3f &up) {
+    Vector3f f = (cen - eye).normalized();
+    Vector3f s = f.cross(up).normalized();
+    Vector3f u = s.cross(f);
+
+    Affine3f trf;
+    trf.matrix() << s[0], s[1], s[2],-s.dot(eye),
+                    u[0], u[1], u[2],-u.dot(eye),
+                   -f[0],-f[1],-f[2], f.dot(eye),
+                    0,    0,    0,    1;
+    return trf;
+  }
+
+  Affine3f lookat_lh(const Vector3f &eye,
+                     const Vector3f &cen,
+                     const Vector3f &up) {
+    Vector3f f = (cen - eye).normalized();
+    Vector3f s = up.cross(f).normalized();
+    Vector3f u = f.cross(s);
+
+    Affine3f trf;
+    trf.matrix() << s[0], s[1], s[2],-s.dot(eye),
+                    u[0], u[1], u[2],-u.dot(eye),
+                    f[0], f[1], f[2],-f.dot(eye),
+                    0,    0,    0,    1;
+    return trf;
+  }
+
+  Projective3f perspective_rh_no(float fovy, float aspect, float near, float far) {
+    const float tan_half_fovy = std::tan(fovy / 2.f);
+    const float _00 = 1.f / (aspect * tan_half_fovy);
+    const float _11 = 1.f / tan_half_fovy;
+    const float _22 = -(far + near) / (far - near);
+    const float _32 = -(2.f * far * near) / (far - near);
+    const float _23 = -1;
+
+    Projective3f trf;
+    trf.matrix() << _00, 0, 0, 0,
+                    0, _11, 0, 0,
+                    0, 0,_22,_32,
+                    0, 0,_23,  0;
+    return trf;
+  }
+} // namespace Eigen
