1be7879464cd8745412421bb2a099db54491675e
Mark van de Ruit <markvanderuit@gmail.com>
2022-08-16 11:47:27 +0200

    Workdump

--- include/metameric/components/views/task_mappings_viewer.hpp
+++ include/metameric/components/views/task_mappings_viewer.hpp
@@ -2,24 +2,36 @@
 
 #include <metameric/core/math.hpp>
 #include <metameric/core/scheduler.hpp>
+#include <metameric/core/spectrum.hpp>
 #include <metameric/core/detail/scheduler_subtasks.hpp>
 #include <metameric/components/tasks/detail/task_texture_resample.hpp>
+#include <small_gl/buffer.hpp>
 #include <small_gl/texture.hpp>
+#include <small_gl/utility.hpp>
 #include <vector>
 
 namespace met {
   class MappingsViewerTask : public detail::AbstractTask {
     using ResampleTaskType = detail::TextureResampleTask<gl::Texture2d4f>;
 
-    eig::Array2u                       m_texture_size = 1;
-    detail::Subtasks<ResampleTaskType> m_resample_subtasks;
+    eig::Array2u                       m_resample_size;
+    detail::Subtasks<ResampleTaskType> m_resample_tasks;
 
-    void handle_tooltip(detail::TaskEvalInfo &info, uint texture_i);
-    void handle_popout(detail::TaskEvalInfo &info, uint texture_i);
+    gl::Buffer                         m_tooltip_buffer;
+    std::span<Spec>                    m_tooltip_map;
+    gl::sync::Fence                    m_tooltip_fence;
+    eig::Array2i                       m_tooltip_pixel;
+    int                                m_tooltip_i;
+
+
+    void eval_tooltip_copy(detail::TaskEvalInfo &info, uint texture_i);
+    void eval_tooltip(detail::TaskEvalInfo &info, uint texture_i);
+    void eval_popout(detail::TaskEvalInfo &info, uint texture_i);
 
   public:
     MappingsViewerTask(const std::string &name);
     
+    void init(detail::TaskInitInfo &) override;
     void dstr(detail::TaskDstrInfo &) override;
     void eval(detail::TaskEvalInfo &) override;
   };
--- src/components/views/task_mappings_viewer.cpp
+++ src/components/views/task_mappings_viewer.cpp
@@ -1,7 +1,5 @@
 #include <metameric/core/state.hpp>
-#include <metameric/core/spectrum.hpp>
 #include <metameric/core/texture.hpp>
-#include <metameric/core/utility.hpp>
 #include <metameric/core/detail/trace.hpp>
 #include <metameric/components/views/task_mappings_viewer.hpp>
 #include <metameric/components/views/detail/imgui.hpp>
@@ -9,7 +7,6 @@
 #include <small_gl/buffer.hpp>
 
 namespace met {
-  constexpr auto gen_subtask_fmt      = FMT_COMPILE("gen_color_mapping_{}");
   constexpr auto gen_subtask_tex_fmt  = FMT_COMPILE("gen_color_mapping_texture_{}");
   constexpr auto resample_subtask_fmt = FMT_COMPILE("mappings_viewer_resample_{}");
 
@@ -31,38 +28,45 @@ namespace met {
     return fmt::format(resample_subtask_fmt, i);
   };
 
-  void MappingsViewerTask::handle_tooltip(detail::TaskEvalInfo &info, uint texture_i) {
+  void MappingsViewerTask::eval_tooltip_copy(detail::TaskEvalInfo &info, uint texture_i) {
     met_trace_full();
 
     // Get shared resources
     auto &e_spectrum_buffer = info.get_resource<gl::Buffer>("gen_spectral_texture", "spectrum_buffer");
-    auto &e_app_data        = info.get_resource<ApplicationData>(global_key, "app_data");
-    auto &e_tex_data        = e_app_data.loaded_texture;
-    auto &e_mapping         = e_app_data.loaded_mappings[texture_i];
+    auto &e_tex_data        = info.get_resource<ApplicationData>(global_key, "app_data").loaded_texture;
 
     // Compute sample position in texture dependent on mouse position in image
     eig::Array2f mouse_pos  =(static_cast<eig::Array2f>(ImGui::GetMousePos()) 
                             - static_cast<eig::Array2f>(ImGui::GetItemRectMin()))
                             / static_cast<eig::Array2f>(ImGui::GetItemRectSize());
-    eig::Array2i sample_pos = (mouse_pos * e_tex_data.size().cast<float>()).cast<int>();
-    const size_t sample_i   = e_tex_data.size().x() * sample_pos.y() + sample_pos.x();
+    m_tooltip_pixel   = (mouse_pos * e_tex_data.size().cast<float>()).cast<int>();
+    const size_t sample_i = e_tex_data.size().x() * m_tooltip_pixel.y() + m_tooltip_pixel.x();
 
-    // Copy reflectance data at sample position from gpu buffer
-    Spec reflectance = cast_span<Spec>(e_spectrum_buffer.map(gl::BufferAccessFlags::eMapRead,
-      sizeof(Spec), sizeof(Spec) * sample_i))[0];
-    e_spectrum_buffer.unmap();
+    // Perform copy of relevant reflectance data to mapped buffer, then submit a fence
+    e_spectrum_buffer.copy_to(m_tooltip_buffer, sizeof(Spec), sample_i * sizeof(Spec));
+    gl::sync::memory_barrier(gl::BarrierFlags::eClientMappedBuffer);
+    m_tooltip_fence = gl::sync::Fence(gl::sync::time_s(1));
+  }
 
-    // Spec reflectance;// m_spectrum_buffer_map[sample_i];
-    // e_spectrum_buffer.get(as_span<std::byte>(reflectance), sizeof(Spec), sizeof(Spec) * sample_i);
+  void MappingsViewerTask::eval_tooltip(detail::TaskEvalInfo &info, uint texture_i) {
+    met_trace_full();
 
-    // Compute output on the fly for said data
-    Spec  power     = e_mapping.apply_power(reflectance);
-    Color power_rgb = e_mapping.apply_color(reflectance);
-
-    // Spawn a simple tooltip showing reflectance, power, rgb values, etc.
+    // Get shared resources
+    auto &e_app_data = info.get_resource<ApplicationData>(global_key, "app_data");
+    auto &e_mapping  = e_app_data.loaded_mappings[texture_i];
+    
+    // Spawn tooltip
     ImGui::BeginTooltip();
-    ImGui::Text("Inspecting pixel (%i, %i)", sample_pos.x(), sample_pos.y());
+    ImGui::Text("Inspecting pixel (%i, %i)", m_tooltip_pixel.x(), m_tooltip_pixel.y());
     ImGui::Separator();
+
+    // Compute output for reflectance data once it is available; no choice but to wait here
+    m_tooltip_fence.cpu_wait();
+    Spec& reflectance = m_tooltip_map[0];
+    Spec  power       = e_mapping.apply_power(reflectance);
+    Color power_rgb   = e_mapping.apply_color(reflectance);
+    
+    // Plot rest of tooltip
     ImGui::PlotLines("Reflectance", reflectance.data(), wavelength_samples, 0,
       nullptr, 0.f, 1.f, { 0.f, 64.f });
     ImGui::PlotLines("Power", power.data(), wavelength_samples, 0,
@@ -73,17 +77,30 @@ namespace met {
     ImGui::EndTooltip();
   }
 
-  void MappingsViewerTask::handle_popout(detail::TaskEvalInfo &info, uint texture_i) {
+  void MappingsViewerTask::eval_popout(detail::TaskEvalInfo &info, uint texture_i) {
     // ...
   }
 
   MappingsViewerTask::MappingsViewerTask(const std::string &name)
   : detail::AbstractTask(name) { }
   
+  void MappingsViewerTask::init(detail::TaskInitInfo &info) {
+    met_trace_full();
+
+    m_resample_size = 1;
+
+    // Initialize a buffer of size Spec, and map it for reading
+    m_tooltip_buffer = {{ .size = sizeof(Spec),
+                          .flags = gl::BufferCreateFlags::eMapPersistent
+                                 | gl::BufferCreateFlags::eMapRead }};
+    m_tooltip_map = cast_span<Spec>(m_tooltip_buffer.map(
+      gl::BufferAccessFlags::eMapPersistent | gl::BufferAccessFlags::eMapRead));
+  }
+  
   void MappingsViewerTask::dstr(detail::TaskDstrInfo &info) {
     met_trace_full();
     
-    m_resample_subtasks.dstr(info);
+    m_resample_tasks.dstr(info);
   }
 
   void MappingsViewerTask::eval(detail::TaskEvalInfo &info) {
@@ -91,33 +108,32 @@ namespace met {
     
     if (ImGui::Begin("Mappings viewer")) {
       // Get shared resources
-      auto &e_spectrum_buffer  = info.get_resource<gl::Buffer>("gen_spectral_texture", "spectrum_buffer");
-      auto &e_app_data         = info.get_resource<ApplicationData>(global_key, "app_data");
-      auto &e_tex_data         = e_app_data.loaded_texture;
-      auto &e_prj_data         = e_app_data.project_data;
-      auto &e_mappings         = e_prj_data.mappings;
-      uint e_mappings_n        = e_app_data.loaded_mappings.size();
+      auto &e_app_data  = info.get_resource<ApplicationData>(global_key, "app_data");
+      auto &e_tex_data  = e_app_data.loaded_texture;
+      auto &e_prj_data  = e_app_data.project_data;
+      auto &e_mappings  = e_prj_data.mappings;
+      uint e_mappings_n = e_app_data.loaded_mappings.size();
 
       // Set up drawing a nr. of textures in a column-based layout; determine texture res.
       uint n_cols = 2;
       eig::Array2f viewport_size = static_cast<eig::Array2f>(ImGui::GetWindowContentRegionMax().x)
                                  - static_cast<eig::Array2f>(ImGui::GetWindowContentRegionMin().x);
-      eig::Array2f texture_scale = viewport_size
+      eig::Array2f texture_size = viewport_size
                                  * e_app_data.loaded_texture.size().y()
                                  / e_app_data.loaded_texture.size().x()
                                  * 0.95f / static_cast<float>(n_cols);
                                  
       // If texture size has changed, respawn texture resample tasks
-      if (auto texture_size = texture_scale.cast<uint>(); !m_texture_size.isApprox(texture_size)) {
+      if (auto resample_size = texture_size.cast<uint>(); !resample_size.isApprox(m_resample_size)) {
         // Reinitialize resample subtasks on texture size change
-        m_texture_size = texture_size;
-        m_resample_subtasks.init(name(), info, e_mappings_n, 
-          resample_subtask_add(m_texture_size), resample_subtask_rmv);
+        m_resample_size = resample_size;
+        m_resample_tasks.init(name(), info, e_mappings_n, 
+          resample_subtask_add(m_resample_size), resample_subtask_rmv);
       } else {
         // Adjust nr. of spawned tasks to correct number
-        m_resample_subtasks.eval(info, e_mappings_n);
+        m_resample_tasks.eval(info, e_mappings_n);
       }
-      
+
       // Iterate n_cols, n_rows, and n_mappings
       for (uint i = 0, i_col = 0; i < e_mappings.size(); ++i) {
         // Generate name of task holding texture data
@@ -131,32 +147,16 @@ namespace met {
         // Draw image
         ImGui::BeginGroup();
         ImGui::Text(e_mappings[i].first.c_str());
-        ImGui::Image(ImGui::to_ptr(e_texture.object()), texture_scale);
-        // ImGui::ImageButton(ImGui::to_ptr(e_texture.object()), texture_scale,
-          // { 0, 0 }, { 1, 1 }, -1, { 0, 0, 0, 0 }, { 0.95, 1, 1, 1 });
+        ImGui::Image(ImGui::to_ptr(e_texture.object()), texture_size);
         
-        // Do pop-out if image is double clicked
-        if (ImGui::IsMouseDoubleClicked(ImGuiMouseButton_Left)) { handle_popout(info, i); }
-        
-        if (ImGui::IsItemHovered()) {
-          // Mouse enters hover state; acquire map over spectral buffer
-          // if (!e_spectrum_buffer.is_mapped()) {
-          //   auto map_flags = gl::BufferAccessFlags::eMapRead | gl::BufferAccessFlags::eMapPersistent;
-          //   m_spectrum_buffer_map = cast_span<Spec>(e_spectrum_buffer.map(map_flags));
-          //   fmt::print("Mapping buffer");
-          // }
-
-          // Show tooltip on mouse-over
-          handle_tooltip(info, i);
-        } else {
-          // Mouse no longer in hover state; release map over spectral buffer
-          // if (e_spectrum_buffer.is_mapped()) {
-          //   e_spectrum_buffer.unmap();
-          //   m_spectrum_buffer_map = { };
-          //   fmt::print("Unmapping buffer");
-          // }
+        // Set id for tooltip after loop is over, and schedule copy of data
+        if (m_tooltip_i = ImGui::IsItemHovered() ? i : -1; m_tooltip_i != -1) { 
+          eval_tooltip_copy(info, m_tooltip_i); 
         }
 
+        // Do pop-out if image is double clicked
+        if (ImGui::IsMouseDoubleClicked(ImGuiMouseButton_Left)) { eval_popout(info, i); }
+        
         ImGui::EndGroup();
         
         // Increment column count; insert same-line if on same row and not last item
@@ -167,6 +167,9 @@ namespace met {
           ImGui::SameLine();
         }
       }
+
+      // Handle tooltip after copy is hopefully completed
+      if (m_tooltip_i != -1) { eval_tooltip(info, m_tooltip_i); }
     }
     ImGui::End();
   }
