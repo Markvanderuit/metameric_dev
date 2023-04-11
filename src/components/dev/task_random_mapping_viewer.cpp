573d8a23808ea50fd8346a08b208228e3b88cb32
Mark van de Ruit <markvanderuit@gmail.com>
2023-04-11 16:40:33 +0200

    Minor bugfix

--- src/components/dev/task_random_mapping_viewer.cpp
+++ src/components/dev/task_random_mapping_viewer.cpp
@@ -59,6 +59,10 @@ namespace met {
         // Get externally shared resources; note, resources may not be created yet as tasks are
         // added into the schedule at the end of a loop, not during
         std::string gen_name = fmt::format("{}.gen_texture_{}", info.task().key(), i); // subtask name
+        if (!info.task(gen_name).is_init()) {
+          ImGui::PopID();
+          continue;
+        }
         guard_continue(info.task(gen_name).is_init());
         const auto &e_texture = info(gen_name, "texture").read_only<gl::Texture2d4f>();
 
