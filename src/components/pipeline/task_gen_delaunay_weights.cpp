ff4fb0f08b0853a657ec95610567d6d68f69054f
Mark van de Ruit <markvanderuit@gmail.com>
2023-04-17 14:47:20 +0200

    Work on very-sketchy-bvh-building

--- src/components/pipeline/task_gen_delaunay_weights.cpp
+++ src/components/pipeline/task_gen_delaunay_weights.cpp
@@ -15,52 +15,180 @@ namespace met {
   constexpr uint buffer_init_size    = 512u;
 
   namespace detail {
-    // FWD
-    struct TreeNode;
-    struct TreeObject;
-    
-    // Delaunay search tree; node data structure
-    struct TreeNode {
-      eig::Array3f b_min;    // Bounding volume minimum
-      uint         e_begin;  // Begin index of underlying element 
-      eig::Array3f b_center; // Bounding volume center; volume max
-      uint         e_extent; // Extent of underlying element range
-    };
-
-    // Delaunay search tree; implict tree object
-    struct TreeObject {
-      uint n_levels;   // Tree depth
-      uint n_children; // Max degree for non-leaf nodes
-
-      std::vector<TreeNode> nodes;
-    };
-
-    template <typename Mesh = IndexedDelaunayData>
-    TreeObject generate_search_tree(Mesh delaunay) {
-      TreeObject object;
-
-      std::vector<TreeNode> leaves(delaunay.elems.size());
-
-      // Build leaf nodes
-      #pragma omp parallel for
-      for (int i = 0; i < leaves.size(); ++i) {
-        const eig::Array4u &el = delaunay.elems[i];
-
-        // Gather vertex data
-        std::array<eig::Array3f, 4> vt;
-        std::ranges::transform(el, vt.begin(), [](uint i) { return delaunay.verts[i]; });
-
-        // Build node data 
-        TreeNode node = { .b_min    = vt[0].min(vt[1].min(vt[2].min(vt[3]))).eval()
-                          .e_begin  = i, 
-                          .e_extent = 1 };
-        node.b_center = (vt[0].max(vt[1].max(vt[2].max(vt[3]))).eval() - node.b_min) * 0.5;        
-        return node;
+    namespace radix {
+      uint expand_bits_10(uint i)  {
+        i = (i * 0x00010001u) & 0xFF0000FFu;
+        i = (i * 0x00000101u) & 0x0F00F00Fu;
+        i = (i * 0x00000011u) & 0xC30C30C3u;
+        i = (i * 0x00000005u) & 0x49249249u;
+        return i;
       }
 
-      
+      uint morton_code(eig::Vector3f v_) {
+        auto v = (v_ * 1024.f).cwiseMax(0.f).cwiseMin(1023.f).eval();
+        uint x = expand_bits_10(uint(v[0]));
+        uint y = expand_bits_10(uint(v[1]));
+        uint z = expand_bits_10(uint(v[2]));
+        return x * 4u + y * 2u + z;
+      }
 
-      return object;
+      void sort() {
+        // TODO: implement
+      }
+    } // namespace radix
+
+    /* Implicit tree helper code */
+
+    // Hardcoded log2 of tree's degree
+    template <uint Degr> consteval uint tree_degr_log();
+    // template <> consteval uint tree_degr_log<2>() { return 1; }
+    // template <> consteval uint tree_degr_log<4>() { return 2; }
+    template <> consteval uint tree_degr_log<8>() { return 3; }
+
+    // Beginning index of node on current tree level
+    template <uint Degr> constexpr uint tree_lvl_begin(int lvl);
+    // template <> constexpr uint tree_lvl_begin<2>(int lvl) { return 0xFFFFFFFF >> (32 - (lvl - 1)); }
+    // template <> constexpr uint tree_lvl_begin<4>(int lvl) { return 0x55555555 >> (31 - ((tree_degr_log<4>() * lvl)) + 1); }
+    template <> constexpr uint tree_lvl_begin<8>(int lvl) { return (0x92492492 >> (31 - tree_degr_log<8>() * lvl)) >> 2; }
+    
+    // Extent of indices on current tree level
+    template <uint Degr> constexpr uint tree_lvl_extent(uint lvl) {
+      return tree_lvl_begin<Degr>(lvl + 1) - tree_lvl_begin<Degr>(lvl);
+    }
+    
+    // Total nr. of nodes including padding, given nr. of tree levels
+    template <uint Degr> constexpr uint tree_n_nodes(uint lvls) { 
+      return tree_lvl_begin<Degr>(lvls + 1); 
+    }
+
+    // Total nr. of levels including padding, given nr. of leaf nodes
+    template <uint Degr> constexpr uint tree_n_lvls(uint leaves) {
+      return 1 + static_cast<uint>(std::ceil(std::log2(leaves) / tree_degr_log<Degr>()));
+    }
+
+    // Delaunay search tree; implict bvh structure
+    template <uint Degr>
+    struct TreeObject {
+      constexpr static uint Degree = Degr; // Maximum degree for non-leaf nodes
+      
+      struct Node {
+        eig::Array3f b_min;    // Bounding volume minimum
+        uint         e_begin;  // Begin index of underlying element 
+        eig::Array3f b_max;    // Bounding volume center; volume max
+        uint         e_extent; // Extent of underlying element range
+      };
+    
+    public:
+      // Default constructor
+      TreeObject() = default;
+      
+      // Sized constructor
+      TreeObject(uint n_objects)
+      : n_objects(n_objects),
+        n_levels(tree_n_lvls<Degr>(n_objects)),
+        nodes(tree_n_nodes<Degr>(n_levels), Node { .e_extent = 0 })
+      { }
+
+      // Public members
+      uint n_levels;
+      uint n_objects;
+      std::vector<Node> nodes;
+        
+      // Node/leaf accessors/iterators
+      size_t node_begin_i(uint lvl) const { return tree_lvl_begin<Degr>(lvl);                              }
+      size_t node_end_i(uint lvl)   const { return tree_lvl_begin<Degr>(lvl) + tree_lvl_extent<Degr>(lvl); }
+      auto   node_begin(uint lvl)   const { return nodes.begin() + node_begin_i(lvl);                      }
+      auto   node_end(uint lvl)     const { return nodes.begin() + node_end_i(lvl);                        }
+      size_t leaf_begin_i()         const { return tree_lvl_begin<Degr>(n_levels - 1);                     }
+      size_t leaf_end_i()           const { return tree_lvl_begin<Degr>(n_levels - 1) + n_objects;         }
+      auto   leaf_begin()           const { return nodes.begin() + leaf_begin_i();                         }
+      auto   leaf_end()             const { return nodes.begin() + leaf_end_i();                           }
+    };
+
+    template <typename Mesh = IndexedDelaunayData, uint Degr>
+    std::pair<Mesh, TreeObject<Degr>> generate_search_tree(Mesh delaunay) {
+      using Tree = TreeObject<Degr>;
+      using Node = Tree::Node; 
+      
+      met_trace();
+
+      // Establish object centers as targets for morton order
+      std::vector<eig::Vector3f> centers(delaunay.elems.size());
+      std::transform(std::execution::par_unseq, range_iter(delaunay.elems), centers.begin(), [&](const eig::Array4u &el) {
+        return ((delaunay.verts[el[0]] + delaunay.verts[el[1]] + 
+                 delaunay.verts[el[2]] + delaunay.verts[el[3]]) / 4.f).eval();
+      });
+
+      // Build quick and dirty morton order
+      std::vector<uint> codes(centers.size());
+      std::vector<uint> order(centers.size());
+      std::transform(std::execution::par_unseq, range_iter(centers), codes.begin(), radix::morton_code);
+      std::iota(range_iter(order), 0u);
+      std::sort(std::execution::par_unseq, range_iter(order), [&](uint i, uint j) { return codes[i] < codes[j]; });
+
+      // Adjust mesh element data to adhere to sorted order
+      auto _elems = delaunay.elems;
+      std::transform(std::execution::par_unseq, range_iter(order), delaunay.elems.begin(), [&](uint i) { return _elems[i]; });
+
+      Tree tree(order.size());
+      /* fmt::print("n_children : {}\n", Tree::Degree);
+      fmt::print("levels     : {}\n", tree.n_levels);
+      fmt::print("n_objects  : {}\n", tree.n_objects);
+      fmt::print("n_nodes    : {}\n", tree.nodes.size()); */
+
+      // Build bottom-most level
+      fmt::print("leaf: {} - {}\n", tree.leaf_begin_i(), tree.leaf_end_i());
+
+      #pragma omp parallel for
+      for (int i = tree.leaf_begin_i(); i < tree.leaf_end_i(); ++i) { // extent of nr. of leaf objects, not nr. of leaf nodes
+        uint i_underlying = i - static_cast<uint>(tree.leaf_begin_i());
+      
+        // Gather vertex data for the current element
+        const eig::Array4u &el = delaunay.elems[i_underlying];
+        std::array<eig::Array3f, 4> vt;
+        std::ranges::transform(el, vt.begin(), [&](uint i) { return delaunay.verts[i]; });
+        
+        // Build node data, wrapping bbox around element
+        tree.nodes[i] = { .b_min    = vt[0].min(vt[1].min(vt[2].min(vt[3]))).eval(),
+                          .e_begin  = i_underlying,
+                          .b_max    = vt[0].max(vt[1].max(vt[2].max(vt[3]))).eval(),
+                          .e_extent = 1 };
+      } // end for i
+
+      // Reduce upwards levels
+      for (int lvl = tree.n_levels - 1; lvl >= 0; lvl--) {
+        fmt::print("level {}: {} - {}\n", lvl, tree.node_begin_i(lvl), tree.node_end_i(lvl));
+
+
+        #pragma omp parallel for
+        for (int i = tree.node_begin_i(lvl); i < tree.node_end_i(lvl); ++i) {
+          // Start with new empty node as reduction target
+          Node node = { .e_extent = 0 };
+
+          uint j_begin = i * Tree::Degree + 1;
+          for (uint j = j_begin; j < j_begin + Tree::Degree; ++j) {
+            const Node &child = tree.nodes[j];
+
+            // Test if either node contains data
+            if (child.e_extent == 0) {
+              continue;
+            } else if (node.e_extent == 0) {
+              node = child;
+              continue;
+            }
+            
+            // Reduce result into new node if both contain data
+            node.e_begin  = std::min(node.e_begin, child.e_begin);
+            node.e_extent = node.e_extent + child.e_extent; 
+            node.b_min    = node.b_min.cwiseMin(child.b_min);
+            node.b_max    = node.b_max.cwiseMax(child.b_max);
+          } // end for j
+
+          tree.nodes[i] = node;
+        } // end for i
+      } // end for lvl
+      
+      return { delaunay, tree };
     }
   } // namespace detail
 
@@ -136,6 +264,8 @@ namespace met {
     std::vector<Colr> delaunay_input(e_proj_data.verts.size());
     std::ranges::transform(e_proj_data.verts, delaunay_input.begin(), [](const auto &vt) { return vt.colr_i; });
     i_delaunay = generate_delaunay<AlignedDelaunayData, Colr>(delaunay_input);
+    detail::TreeObject<8> tree;
+    std::tie(i_delaunay, tree) = detail::generate_search_tree<AlignedDelaunayData, 8>(i_delaunay);
 
     // Recover triangle element data and store in project
     auto [_, elems] = convert_mesh<AlignedMeshData>(i_delaunay);
