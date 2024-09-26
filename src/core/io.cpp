724472f5a6b2dcf5aeeac487f85577882bd5beba
Mark van de Ruit <markvanderuit@gmail.com>
2024-09-26 17:10:13 +0200

    Added rapidobj to cmake

--- src/core/io.cpp
+++ src/core/io.cpp
@@ -6,6 +6,7 @@
 #include <assimp/scene.h>
 #include <assimp/postprocess.h>
 #include <nlohmann/json.hpp>
+#include <rapidobj/rapidobj.hpp>
 #include <algorithm>
 #include <deque>
 #include <execution>
