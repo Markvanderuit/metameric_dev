466e744b113045a332b6ba175b3462a20419a2e8
Mark van de Ruit <markvanderuit@gmail.com>
2024-11-18 00:08:50 +0100

    Minor compatibility fixes for llvm

--- include/metameric/core/ranges.hpp
+++ include/metameric/core/ranges.hpp
@@ -16,4 +16,11 @@ namespace met {
   inline constexpr auto index_into_view = [](rng::viewable_range auto &&r) {
     return vws::transform([r = rng::ref_view { r }](unsigned i) { return r[i]; });
   };
+
+  // Replacement for std::ranges::to given lacking support on some compilers
+  template<typename VTy>
+  inline constexpr auto view_to = [](rng::viewable_range auto &&r) {
+    using Ty = std::decay_t<rng::range_value_t<R>>;
+    return VTy<Ty>(r.begin(), r.end());
+  }e
 } // namespace met
